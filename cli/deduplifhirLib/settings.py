e978bbe6a3d3b019d1c99df3f457a1650b4cc5a7
Isaac Milarsky <isaac.milarsky@hhs.cms.gov>
2024-04-30 16:21:19 -0500

    add settings file to make it easier to adjust blocking rules
    Signed-off-by: Isaac Milarsky <isaac.milarsky@hhs.cms.gov>

--- cli/deduplifhirLib/settings.py
+++ cli/deduplifhirLib/settings.py
@@ -11,6 +11,7 @@ same first and last name in the input in order to find confirmed matches
 The comparisons list defines the way the model will compare the data in order to find
 the probability that records are duplicates of each other. 
 """
+import os
 import json
 import uuid
 import pandas as pd
@@ -19,14 +20,12 @@ import splink.duckdb.comparison_template_library as ctl
 from splink.duckdb.blocking_rule_library import block_on
 
 
-SPLINK_LINKER_SETTINGS_PATIENT_DEDUPE = {
-    "link_type": "dedupe_only",
-    "blocking_rules_to_generate_predictions": [
-        block_on( "birth_date"),
-        block_on(["ssn", "birth_date"]),
-        block_on(["ssn", "street_address"]),
-        block_on("phone"),
-    ],
+dir_path = os.path.dirname(os.path.realpath(__file__))
+with open(dir_path + '/splink_settings.json',"r",encoding="utf-8") as f:
+    SPLINK_LINKER_SETTINGS_PATIENT_DEDUPE = json.load(f)
+
+
+SPLINK_LINKER_SETTINGS_PATIENT_DEDUPE.update({
     "comparisons": [
         ctl.name_comparison("given_name", term_frequency_adjustments=True),
         ctl.name_comparison("family_name", term_frequency_adjustments=True),
@@ -34,12 +33,12 @@ SPLINK_LINKER_SETTINGS_PATIENT_DEDUPE = {
         ctl.postcode_comparison("postal_code"),
         cl.exact_match("street_address", term_frequency_adjustments=True),
         cl.exact_match("phone",  term_frequency_adjustments=True),
-    ],
-    "retain_matching_columns": True,
-    "retain_intermediate_calculation_columns": True,
-    "max_iterations": 20,
-    "em_convergence": 0.01
-}
+    ]
+})
+
+#apply blocking function to translate into sql rules
+blocking_rules = SPLINK_LINKER_SETTINGS_PATIENT_DEDUPE["blocking_rules_to_generate_predictions"]
+SPLINK_LINKER_SETTINGS_PATIENT_DEDUPE["blocking_rules_to_generate_predictions"] = list(map(block_on,blocking_rules))
 
 
 #NOTE: The only reason this function is defined outside utils.py is because of a known bug with
