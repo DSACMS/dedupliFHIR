e6f4bc9803153755806227a0ff46f043f2e14d98
Natalia Luzuriaga <natalia.luzuriaga@cms.hhs.gov>
2024-09-04 13:28:24 -0700

    added instructions to success page
    Signed-off-by: Natalia Luzuriaga <natalia.luzuriaga@cms.hhs.gov>

--- frontend/renderer/renderer-success.js
+++ frontend/renderer/renderer-success.js
@@ -1,4 +1,7 @@
 const successAlert = document.getElementById("download-success-alert");
+const instructions = document.getElementById(
+  "results-spreadsheet-instructions",
+);
 const failAlert = document.getElementById("download-fail-alert");
 
 const downloadButton = document.getElementById("download");
@@ -6,9 +9,11 @@ downloadButton.addEventListener("click", async () => {
   const filePath = await window.electronAPI.saveFile();
   if (filePath) {
     successAlert.style.display = "block";
+    instructions.style.display = "block";
     failAlert.style.display = "none";
   } else {
     successAlert.style.display = "none";
+    instructions.style.display = "none";
     failAlert.style.display = "block";
   }
 });
