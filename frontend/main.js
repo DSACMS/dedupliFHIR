7f58499a9b234bdba085fb070fbd3d6a4b34dc24
Natalia Luzuriaga <natalia.luzuriaga@cms.hhs.gov>
2024-09-04 14:23:22 -0700

    adjusted window size
    Signed-off-by: Natalia Luzuriaga <natalia.luzuriaga@cms.hhs.gov>

--- frontend/main.js
+++ frontend/main.js
@@ -117,6 +117,8 @@ async function handleSaveFile() {
 
 function createWindow() {
   mainWindow = new BrowserWindow({
+    width: 900, 
+    height: 700,
     webPreferences: {
       preload: path.join(__dirname, "./preload.js"),
       contextIsolation: true,
