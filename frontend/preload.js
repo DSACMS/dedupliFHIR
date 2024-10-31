e19d79d8f5a96cdd77029ca411d852460cbdf8a5
Natalia Luzuriaga <natalia.luzuriaga@cms.hhs.gov>
2024-10-31 15:44:40 -0700

    Added pathForFile func to get file path using webutils
    Signed-off-by: Natalia Luzuriaga <natalia.luzuriaga@cms.hhs.gov>

--- frontend/preload.js
+++ frontend/preload.js
@@ -1,6 +1,7 @@
-const { ipcRenderer, contextBridge } = require("electron");
+const { ipcRenderer, contextBridge, webUtils } = require("electron");
 
 contextBridge.exposeInMainWorld("electronAPI", {
+  pathForFile: (file) => webUtils.getPathForFile(file),
   runProgram: (filePath, fileFormat) =>
     ipcRenderer.invoke("runProgram", filePath, fileFormat),
   saveFile: () => ipcRenderer.invoke("dialog:saveFile"),
